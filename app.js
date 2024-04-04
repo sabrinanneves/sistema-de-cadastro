b8f313ae176918aa23f6844251e7e789ba51a448
Sabrina Neves <sabrina.kanashiro.ext@dasa.com.br>
2024-04-04 18:34:02 -0300

    restore source

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 
 console.log("testando git commit");
-// //Cd = pasta
-// Ls = ver arquivos dentro de uma pasta antes vamos acessar a pasa cd sistema-de-cadastro/ e com o ls veremos o nome dos arquivos dentro dessa pasta 
-
-// git clone = clonar projeto
-// git log =  ver o historico de alteracoes recentes. ele mostra dois pontos no final para sair e so clciar na tecla Q
-// Git log —online = mostra todos os commits
-// git pull: puxar as alteracoes feitas no projetos na minha maquina
-// git status =  status para ver se algo foi modificado
-// Git commit <nome do arquivo> - m “”= comitar
-// git commit . -m"testing git comands”  = outra forma de mandar todos os commits de todos os arquivos que alterou
-// Git push =. empurrar. realizar depois do comia
-// Git push origin main: escolher a main que quer empurrar
-// git restore —source 7474646 . = voltar para um determindado potno da aplicação dps idsso tem que dar commit e push
